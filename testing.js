bc3e2e439ceb1e4c7be1f4bfd56c6bc43a788270
Mohit Aggarwal <mohit.aggarwal@Escalon.com>
2023-10-27 15:20:39 +0530

    revision

---
+++ testing.js
@@ -0,0 +1,19 @@
+/***
+ * HOISTING and CLOSURE
+ */
+
+function hoisting () {
+    function x() {
+        let a = 100;
+        
+        function y() {
+            console.log(a);
+        }
+        y();
+    }
+    x();
+};
+
+const hoist_variable = 'demo';
+
+const a = () => 'Arrow functions enact as variables and get "undefined" during the memory creation phase while functions actually get run';
